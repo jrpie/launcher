f2cd38713e22a7cbdea70727e2e14956e22b055d
Josia Pietsch <git@jrpie.de>
2025-09-22 18:20:49 +0200

    Merge #164 - Migrate build configuration from Groovy to Kotlin
    Co-authored-by: Luke Wass <wassupluke@gmail.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,110 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "de.jrpie.android.launcher"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "de.jrpie.android.launcher"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 49
+        versionName = "0.2.4"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+    }
+
+    val distributionDimension = "distribution"
+    flavorDimensions += distributionDimension
+
+    productFlavors {
+        create("default") {
+            dimension = distributionDimension
+            isDefault = true
+            buildConfigField("boolean", "USE_ACCESSIBILITY_SERVICE", "true")
+        }
+        create("accrescent") {
+            dimension = distributionDimension
+            applicationIdSuffix = ".accrescent"
+            versionNameSuffix = "+accrescent"
+            buildConfigField("boolean", "USE_ACCESSIBILITY_SERVICE", "false")
+        }
+    }
+
+    sourceSets {
+        this.getByName("accrescent") {
+            this.java.srcDir("src/accrescent")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = false
+        dataBinding = true
+        viewBinding = true
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
+    packaging {
+        resources.excludes.addAll(
+            listOf(
+                "META-INF/LICENSE.md",
+                "META-INF/NOTICE.md",
+                "META-INF/LICENSE-notice.md"
+            )
+        )
+    }
+
+    lint {
+        abortOnError = false
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.gridlayout)
+    implementation(libs.androidx.palette.ktx)
+    implementation(libs.androidx.preference.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.google.material)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.jonahbauer.android.preference.annotations)
+    annotationProcessor(libs.jonahbauer.android.preference.annotations)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+}
