8a5c66c04a078411ba3e4f11b8f43723437f807d
Josia Pietsch <git@jrpie.de>
2026-03-09 21:40:50 +0100

    0.2.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "de.jrpie.android.launcher"
         minSdk = 21
         targetSdk = 36
-        versionCode = 55
-        versionName = "0.2.10"
+        versionCode = 56
+        versionName = "0.2.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
